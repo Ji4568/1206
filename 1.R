0907b0146a71f31862925761539d130e0c7d9d62
le402n24\uer <ji8058882@gmail.com>
2023-12-06 16:54:27 +0800

    擷取批踢踢文章

--- 1.R
+++ 1.R
@@ -73,3 +73,27 @@ website = read_html(URL)
 needed_txt = website %>% html_nodes("tr") %>% html_text()
 needed_txt
 
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+
+needed_txt = needed_html %>% html_text()
+needed_txt
+
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
+
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+needed_txt = needed_html %>% html_text()
+needed_txt
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
+
